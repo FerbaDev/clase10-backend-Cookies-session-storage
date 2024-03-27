7050ebafb3b80365a61dabcea5feef52a7e52367
FerbaDev <elferbarron1@gmail.com>
2024-03-27 01:59:54 -0300

    cookies firmadas

--- README.md
+++ README.md
@@ -8,6 +8,11 @@ setear una cookie -------------------- 00:30
 leer el valor de una cookie ---------- 00:33
 eliminar cookie ---------------------- 00:36
 
+tiempo de vida de la cookie ---------- 00:39
+agregando seguridad con signed cookies 00:45
+obtener y leer la cookie firmada ----- 00:53
+
+
 
 
 configurar cookie parser
--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ const PUERTO = 8080;
 import cookieParser from "cookie-parser";
 
 //middleware para cookie parser
-app.use(cookieParser())
+const PASSWORD = "clavesecreta";
+app.use(cookieParser(PASSWORD));
+
 
 //rutas
 app.get("/", (req, res) => {
@@ -13,8 +15,8 @@ app.get("/", (req, res) => {
 
 //ruta para setear una cookie
 app.get("/setcookie", (req, res) => {
-    //usamos el obj res para asignarle la cookie al cliente
-    res.cookie("primerCookie", "Mi primera cookie").send("Cookie seteada")
+    //usamos el obj res para asignarle la cookie al cliente, se guardan en formato clave valor. siqueremos que las cookies tengan tiempo de vida agrgamos el objeto con una configuracion de maxAge en milisegundos.
+    res.cookie("primerCookie", "Mi primera cookie", {maxAge: 10000}).send("Cookie seteada")
 })
 
 //leer el valor de una cookie
@@ -25,6 +27,23 @@ app.get("/leercookie", (req, res) => {
 //borrar cookie
 app.get("/borrarcookie", (req, res) => {
     res.clearCookie("primerCookie").send("Cookie eliminada")
+})
+
+//enviar una cooke firmada 
+app.get("/cookiefirmada", (req, res) => {
+    res.cookie("cookieFirmada", "Esta es la firma secreta", {signed: true}).send("Cookie firmada enviada")
+})
+
+//obtenemos la cookie firmada
+app.get("/vercookiefirmada", (req, res) => {
+    //para recuperar las cookies firmada hay que usar req.signedCookies
+    //hacemos u condicional para saber si la cookie fue alterada
+    const valorCookie = req.signedCookies.cookieFirmada;
+    if (valorCookie) {
+        res.send("Cookie recuperada: " + valorCookie)
+    } else {
+        res.send("Cookie invalida")
+    }
 })
 
 //listen
