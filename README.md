11bd2e33f4a68a4a6717c26cc6f53f2fed31dfd5
FerbaDev <elferbarron1@gmail.com>
2024-03-27 01:20:28 -0300

    set cookie, leer cookie y eliminar

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ inicia proyecto ---------------------- 00:10
 instalando cookie parser ------------- 00:25
 levantamos servidor y creamos app ---- 00:26
 
+setear una cookie -------------------- 00:30
+leer el valor de una cookie ---------- 00:33
+eliminar cookie ---------------------- 00:36
+
+
+
 configurar cookie parser
 
 () => {}
--- src/app.js
+++ src/app.js
@@ -6,10 +6,28 @@ import cookieParser from "cookie-parser";
 //middleware para cookie parser
 app.use(cookieParser())
 
+//rutas
 app.get("/", (req, res) => {
     res.send("conecta")
 })
 
+//ruta para setear una cookie
+app.get("/setcookie", (req, res) => {
+    //usamos el obj res para asignarle la cookie al cliente
+    res.cookie("primerCookie", "Mi primera cookie").send("Cookie seteada")
+})
+
+//leer el valor de una cookie
+app.get("/leercookie", (req, res) => {
+    res.send(req.cookies);
+})
+
+//borrar cookie
+app.get("/borrarcookie", (req, res) => {
+    res.clearCookie("primerCookie").send("Cookie eliminada")
+})
+
+//listen
 app.listen(PUERTO, () => {
     console.log(`Conectado a http://localhost:${PUERTO}`);
 })
